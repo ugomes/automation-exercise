1de3ddbf1cd96ea8d845e53359ebe3e14704bbeb
Uelton Gomes <102355027+ugomes@users.noreply.github.com>
2025-05-17 18:21:04 -0300

    Tesr Case 6 Finalizado

---
+++ cypress/e2e/web/test/TestCase6ContactUsForm.cy.js
@@ -0,0 +1,23 @@
+describe('Test Case 6: Contact Us Form', () => {    
+    it('Test case 6: Contact Us Form', () => {
+        cy.visit('https://automationexercise.com/');
+        cy.title().should('eq', 'Automation Exercise');
+        cy.get('a[href="/contact_us"]').click()
+        cy.get('h2').contains('Contact Us').should('be.visible')
+        cy.get('[data-qa=name]').type('John Doe')
+        cy.get('[data-qa=email]').type('example@test.com')
+        cy.get('[data-qa=subject]').type('Test Subject')
+        cy.get('[data-qa=message]').type('This is a test message')
+        cy.get('input[type=file]').selectFile('cypress/fixtures/download (3).png')
+        cy.get('[data-qa="submit-button"]').click()
+        
+        cy.on('window:confirm', (text) => {
+      expect(text).to.equal('Press OK to proceed!')
+      return true; // Clica em "OK"
+        });
+        cy.get('.status.alert.alert-success')
+        .should('have.text', 'Success! Your details have been submitted successfully.')
+
+})
+    
+});
